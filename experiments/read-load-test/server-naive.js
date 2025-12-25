813e23acae79862db43c057eb264f7d86ac1d556
HerizoRand <hontyherizorandrianarisoa@yahoo.com>
2025-12-25 17:20:28 +0300

    feat(server): add route with cached and use redis

--- experiments/read-load-test/server-naive.js
+++ experiments/read-load-test/server-naive.js
@@ -2,6 +2,11 @@ const fastify = require("fastify")({
   logger: true
 })
 const pool = require("./../../db/Pool")
+const { createClient } = require("redis");
+const client = createClient()
+
+client.on('error', err => console.log('Redis Client Error', err))
+client.connect()
 
 fastify.get('/', function (request , reply) {
   reply.send({ hello: 'world'})
@@ -20,6 +25,49 @@ fastify.get('/user/:id', async (request, reply) => {
   console.log(result.rows)
   return result.rows
 })
+
+fastify.get('/user/cached/:id', async (request, reply) => {
+  const {id} = request.params
+  const cacheKey = `user:${id}`
+
+  let cachedData = null
+
+  try {
+    const cachedData = await client.get(cacheKey);
+  } catch (err) {
+    request.log.error({ err }, "Redis is down, falling back to DB");
+  }
+
+  if(cachedData) {
+    reply.header('X-Cache', 'HIT')
+    return JSON.parse(cachedData)
+  }
+
+  const result = await pool.query(
+    `SELECT users.*, messages.content
+    FROM users LEFT JOIN messages
+    ON users.id = messages.user_id
+    WHERE users.id = $1
+    `,
+    [id]
+  );
+
+  try {
+    // Idem pour l'écriture : si ça échoue, tant pis, l'utilisateur a quand même ses données
+    await client.set(cacheKey, JSON.stringify(result.rows), { EX: 60 });
+  } catch (err) {
+    request.log.error({ err }, "Could not save to Redis");
+  }
+  reply.header("X-Cache", "MISS");
+  return result.rows
+  
+})
+
+fastify.addHook("onClose", async (instance) => {
+  console.log("Shutting down connections...");
+  await client.quit();
+  await pool.end();
+});
 
 fastify.listen({ port: 3000, host: '0.0.0.0'}, function (err, address) {
   if (err) {
